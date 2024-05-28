1f2dde61207ad51f71f00e1a6962dc9dd7496354
rekca1457 <rekca@tutamail.com>
2024-05-28 23:05:59 +0300

    Add new feature for user preferences

---
+++ maeri/compiler/nodes/__init__.py
@@ -0,0 +1,17 @@
+from .Add import Add
+from .Conv2 import Conv2
+from .Input import Input
+from .Memory import Memory
+from .Output import Output
+from .Relu import Relu
+from .Root import Root
+from .Result import Result
+
+__all__ = [
+    "Add",
+    "Conv2",
+    "Input",
+    "Memory",
+    "Output",
+    "Relu"
+    ]
